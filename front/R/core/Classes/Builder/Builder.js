040c2d8a3f9963c85c8fed7ad0391e2673fdf116
Viktor Khodosevich <bx7kvs@gmail.com>
2017-04-26 23:28:43 +0300

    Modular Core 0.1
    Modular core basic structure.
    New Injection algorythm untested realization.

--- front/R/core/Classes/Builder/Builder.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Viktor Khodosevich on 3/27/2017.
- */
-$R.build(function (injector,manager,members) {
-    
-});
---
+++ front/R/core/modules/Injector.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Viktor Khodosevich on 4/26/2017.
+ */
